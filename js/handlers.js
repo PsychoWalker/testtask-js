369398de67b7ed162aca6d9891d7a1f0f759e8b1
PsychoWalker <online-ss4@yandex.ru>
2017-04-17 16:41:32 +0400

    to do list

---
+++ js/handlers.js
@@ -0,0 +1,90 @@
+(() => {
+    let buttonAdd = document.querySelector('#add-new-item');
+    let input = document.querySelector('#new-item-name');
+    let list = document.querySelector('.items-list');
+    
+    function toggleComplete(item, isComplete) {
+        if (isComplete) {
+            item.classList.add('completed');
+        } else {
+            item.classList.remove('completed');
+        }
+    }
+    
+    function createItem(name) {
+        let item = document.createElement('div');
+        item.classList.add('list-item');
+        
+        let accept = document.createElement('input');
+        accept.type = 'checkbox';
+        accept.classList.add('accept-button');
+        accept.addEventListener('change', (event) => {
+            toggleComplete(item, accept.checked);
+        });
+        
+        let label = document.createElement('div');
+        label.classList.add('item-name');
+        label.textContent = name;
+        label.addEventListener('dblclick', (event) => {
+            item.classList.add('edit-mode');
+        });
+        
+        let edit = document.createElement('input');
+        edit.type = 'text';
+        edit.value = name;
+        edit.classList.add('edit-name');
+        edit.addEventListener('blur', (event) => {
+            label.textContent = edit.value;
+            item.classList.remove('edit-mode');
+        });
+        edit.addEventListener('keydown', (event) => {
+            
+            if (event.keyCode === 13) {
+                label.textContent = edit.value;
+                item.classList.remove('edit-mode');
+            }
+            
+            if (event.keyCode === 27) {
+                edit.value = label.textContent;
+                item.classList.remove('edit-mode');
+            }
+            
+        });
+        
+        let deleteButton = document.createElement('a');
+        deleteButton.classList.add('delete-item');
+        deleteButton.textContent = 'удалить';
+        deleteButton.href = '';
+        deleteButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            list.removeChild(item);
+        });
+        
+        
+        item.appendChild(accept);
+        item.appendChild(label);
+        item.appendChild(edit);
+        item.appendChild(deleteButton);
+        return item;
+    }
+    
+    function addItem() {
+        let newItem = createItem(input.value);
+        input.value = '';
+        
+        // добавляем дочерний элемент к родительскому
+        list.appendChild(newItem)
+    }
+    
+    // добавим обработчик на кнопку "добавить"
+    buttonAdd.addEventListener('click', addItem);
+
+    // добавим обработчик на "завершить все"
+    document.querySelector('#complete-all').addEventListener('change', (event) => {
+        let allItems = document.querySelectorAll('.list-item');
+        allItems.forEach((item) => {
+            toggleComplete(item, event.target.checked);
+            item.querySelector('.accept-button').checked = event.target.checked;
+        });
+    });
+})();
